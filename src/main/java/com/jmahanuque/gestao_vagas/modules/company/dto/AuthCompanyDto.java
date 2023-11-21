ab43b10ce583d25f819cba9c03a07abf79bfb153
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-11-21 20:55:33 +0200

    AuthCompany Dto create, findByUsername on company repository and checking if the passwords matches

---
+++ src/main/java/com/jmahanuque/gestao_vagas/modules/company/dto/AuthCompanyDto.java
@@ -0,0 +1,12 @@
+package com.jmahanuque.gestao_vagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDto {
+    
+    private String password;
+    private String username;
+}
--- src/main/java/com/jmahanuque/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/jmahanuque/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,8 +1,37 @@
 package com.jmahanuque.gestao_vagas.modules.company.useCases;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.jmahanuque.gestao_vagas.modules.company.dto.AuthCompanyDto;
+import com.jmahanuque.gestao_vagas.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
     
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    public void execute(AuthCompanyDto authCompanyDto) throws AuthenticationException {
+        var company = this.companyRepository.findByUsername(authCompanyDto.getUsername()).orElseThrow(
+            () -> {
+                throw new UsernameNotFoundException("Company not found");
+            });
+
+        // Verify if passwords are same
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDto.getPassword(), company.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        
+    }
 }
